ea9b89733c3bee7a2221708c79cbdea6af319716
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2021-11-17 13:42:29 +0000

    Add project files.

---
+++ NBMMessagingApp/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBMMessagingApp
+{
+    class Message
+    {
+
+        public string messageHeader { get; set; }
+        public string messageSubject { get; set; }
+        public string messageBody { get; set; }
+
+    }
+}
