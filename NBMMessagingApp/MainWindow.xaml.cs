96fe99bb6e64dea7a79758ab613dfe3d0f3e33c4
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2021-11-18 02:33:15 +0000

    Version 1.0
    Early test version.
    
    FEATURES:
    - Inputting of Messages via WPF
    - Message categorisation (E, S, T)
    - Test display to view output
    
    Date/Time: 18/11/21 @ 02:28

--- NBMMessagingApp/MainWindow.xaml.cs
+++ NBMMessagingApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +22,46 @@ namespace NBMMessagingApp
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        List<Message> msgList = new List<Message>();
+
         public MainWindow()
         {
+
             InitializeComponent();
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+
+        }
+
+        private void sendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+
+            
+            string msgSender = senderTextBox.Text;
+            string msgSubject = subjectTextBox.Text;
+            string msgBody = bodyTextBox.Text;
+            int msgID = rnd.Next();
+
+            msgList.Add(new Message(msgSender, msgSubject, msgBody, msgID));
+
+        }
+
+        private void endSessionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void testDisplayButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            foreach (Message msgNum in msgList)
+            {
+                
+                testDisplayMessage.Text = msgNum.getData();
+
+            }
         }
     }
 }
--- NBMMessagingApp/Message.cs
+++ NBMMessagingApp/Message.cs
@@ -7,9 +7,42 @@ namespace NBMMessagingApp
     class Message
     {
 
-        public string messageHeader { get; set; }
+        public string messageSender { get; set; }
         public string messageSubject { get; set; }
         public string messageBody { get; set; }
+        public string messageType { get; set; }
+        public int messageID { get; set; }
+
+        public Message(string msgsender, string msgsubject, string msgbody, int msgID)
+        {
+            this.messageSender = msgsender;
+            this.messageSubject = msgsubject;
+            this.messageBody = msgbody;
+            this.messageID = msgID;
+
+            if (!string.IsNullOrEmpty(messageSubject) & msgsender.Contains("@"))
+            {
+                this.messageType = "E";
+            }
+            else if (msgsender.Contains("+44"))
+            {
+                this.messageType = "S";
+            } else if (msgsender.Contains("@"))
+            {
+                this.messageType = "T";
+            }
+        }
+
+        public string getData()
+        {
+            if (string.IsNullOrEmpty(messageSubject))
+            {
+                return "Sender: " + this.messageSender + "\n\n" + "Body: " + "Message ID:" + this.messageType + this.messageID + "\n\n" + this.messageBody;
+            } else
+            {
+                return "Sender: " + this.messageSender + "\n\n" + "Subject: " + this.messageSubject + "\n\n" + "Message ID:" + this.messageType + this.messageID + "\n\n" + this.messageBody;
+            }            
+        }
 
     }
 }
